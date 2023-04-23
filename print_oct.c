62c3dd16d92e99c08bb543ed198e1f350015ba55
SaniBrian <sanibrian19@gmail.com>
2023-04-23 12:49:32 -0700

    Added print_oct function

---
+++ print_oct.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*print_oct- prints octal specifier
+*@args: current argument
+*Return: print size
+*/
+int print_oct(va_list args)
+{
+	int size;
+	char *buff;
+	char *buffer;
+
+	buff = num_base(va_arg(args, unsigned int), 8);
+	if (buff != NULL)
+	{
+		buffer = buff;
+	} else
+	{
+		buffer = "NULL";
+	}
+
+	for (size = 0; buffer[size] != '\0'; size++)
+	{
+		_putchar(buffer[size]);
+	}
+	return (size);
+}
