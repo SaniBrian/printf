371b28f6b4487e1cfb812ef9a906af7e0ad67230
Ayowale <haydexwisdom@gmail.com>
2023-04-23 18:30:10 +0000

    print_char function updated

--- print_char.c
+++ print_char.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
 *print_char - prints char specifier
 *@args: current argument
@@ -9,7 +9,9 @@
 int print_char(va_list args)
 {
 	int ch = va_arg(args, int);
-
+	
+	if (ch == NULL)
+		return (0);
 	_putchar(ch);
 	return (1);
 }
