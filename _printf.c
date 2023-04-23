389325f04afb1bae9515ab6cf6327bcfe19de575
SaniBrian <sanibrian19@gmail.com>
2023-04-23 10:31:33 -0700

    Added _printf.c function

---
+++ _printf.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "main.h"
+
+/**
+*_printf - produces output according to a format
+*@format: format string
+*Return: buffer printed size
+*/
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int printed = 0, printed_specifier, i;
+
+	va_start(args, format);
+	if (format == 0)
+		return (-1);
+	for (i = 0; format[i] != 0; i++)
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			printed += 1;
+		} else
+		{
+			printed_specifier = handler(format, args, &i);
+			if (printed_specifier == -1)
+				return (-1);
+			printed += printed_specifier;
+		}
+	}
+	va_end(args);
+	return (printed);
+}
