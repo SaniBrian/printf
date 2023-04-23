5ed10bf10efde6da5f25eefe0f2c5016ad9c9f4b
SaniBrian <sanibrian19@gmail.com>
2023-04-23 10:50:54 -0700

    Added print_char function

---
+++ print_char.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*print_char - prints char specifier
+*@args: current argument
+*Return: print size
+*/
+
+int print_char(va_list args)
+{
+	int ch = va_arg(args, int);
+
+	_putchar(ch);
+	return (1);
+}
