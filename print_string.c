dfb3a52ef41621f0489a8c9cbe0bd8ed6824fc78
Ayowale <haydexwisdom@gmail.com>
2023-04-23 17:51:59 +0000

    added the string function

---
+++ print_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*print_string - prints string specifier
+*@args: current argument
+*Return: print size
+*
+*/
+
+int print_string(va_list args)
+{
+	char *str;
+	char *ptr;
+	int size;
+
+	ptr = va_arg(args, char *);
+	if (ptr != NULL)
+	{
+		str = ptr;
+	} else
+	{
+		str = "NULL";
+	}
+	for (size = 0; str[size] != '\0'; size++)
+	{
+		_putchar(str[size]);
+	}
+	return (size);
+}
